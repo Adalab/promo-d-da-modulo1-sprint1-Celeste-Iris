32740dc04fb42f4527627b23e14dfc2623d78482
celeslombardi <celeslombardi@outlook.com>
2023-02-27 16:46:19 +0100

    terminado

--- creacion-de-tablas2.sql
+++ creacion-de-tablas2.sql
@@ -26,6 +26,19 @@ CREATE TABLE facturas (
 	id_factura INT NOT NULL AUTO_INCREMENT,
     numero_factura VARCHAR(45) NOT NULL,
     fecha DATE NOT NULL,
+    id_zapatilla INT NOT NULL,
+    id_empleado INT NOT NULL,
+    id_cliente INT NOT NULL,
     PRIMARY KEY (id_factura),
-    CONSTRAINT `fk_zapatilla_clientes_empleados_facturas`
-		)    
+    CONSTRAINT `fk_facturas_clientes`
+		FOREIGN KEY (id_cliente)
+        REFERENCES clientes (id_cliente) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT `fk_facturas_zapatilla`
+		FOREIGN KEY (id_zapatilla)
+        REFERENCES zapatilla (id_zapatilla) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT `fk_factura_empleado`
+		FOREIGN KEY (id_empleado)
+        REFERENCES empleado (id_empleado) ON DELETE CASCADE ON UPDATE CASCADE);
+        
+        
+        
